34a3e667b9ad1675d5f3eeae52f921319a864057
ziliwassim47-create <ziliwassim47@gmail.com>
2026-04-23 15:58:10 +0100

    ameliore cote admin

---
+++ tables.sql
@@ -0,0 +1,12 @@
+stmt
+SHOW CREATE TABLE ai_analyses;
+SHOW CREATE TABLE ai_chat_history;
+SHOW CREATE TABLE ai_reports;
+SHOW CREATE TABLE community_posts;
+SHOW CREATE TABLE daily_logs;
+SHOW CREATE TABLE food_items;
+SHOW CREATE TABLE meals;
+SHOW CREATE TABLE nutrition_targets;
+SHOW CREATE TABLE post_responses;
+SHOW CREATE TABLE profiles;
+SHOW CREATE TABLE users;
